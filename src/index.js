f02d770480211f8c2f830b6caa9e6094261aa266
Wataru Maeda <WataruMaeda@users.noreply.github.com>
2018-06-26 18:45:24 -0700

    chore: create common

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
-import './polyfill'
+import './common/polyfill';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 // disable ServiceWorker
-// import registerServiceWorker from './registerServiceWorker';
+// import registerServiceWorker from './common/registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 // disable ServiceWorker
